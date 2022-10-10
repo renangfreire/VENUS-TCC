802cd7757372952217bd8fef635811b2b7959a68
KaykyF <kaykyfernandes633@gmail.com>
2022-10-09 21:46:47 -0300

    adjust CSS pages login, register, cart

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import Modal from './modal.js'
 //import ProductComponent from './pg-product.js'
 
 const modal = Modal()
 //const productComponent = ProductComponent()
-=======
-import Modal from './modal.js';
-
-const modal = Modal()
->>>>>>> a05ec7295d0b195acea4ac6d84aadfb6e36ec17f
 const buttonOpenModal = document.querySelector('#modalOpen')
 const button = document.querySelectorAll('.btnColor')
- 
 
 buttonOpenModal.addEventListener('click', modal.open)
--- src/controller/CartController.js
+++ src/controller/CartController.js
@@ -1,26 +1,33 @@
 const FindProductService = require('../service/FindProductService')
 
 module.exports = {
-    async handle(req, res){
-        // res.cookie('cart', JSON.stringify({products: [{id: "37422705"}, {id: '26972801'}]}), { expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)})
-        let products
-        if(req.cookies.cart != null){
-        const arrayList = JSON.parse(req.cookies.cart).products;
-        
-        const idArray = []
+  async handle(req, res) {
+    res.cookie(
+      'cart',
+      JSON.stringify({ products: [{ id: '03408245' }, { id: '68624231' }] }),
+      { expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) }
+    )
+    let products
+    if (req.cookies.cart != null) {
+      const arrayList = JSON.parse(req.cookies.cart).products
 
-        
-        arrayList.map(function(product){
-            idArray.push(product.id)
-        });        
-        
-        const findProductService = new FindProductService();
+      const idArray = []
 
-        products = await findProductService.execute({idProduct: idArray})
+      arrayList.map(function (product) {
+        idArray.push(product.id)
+      })
 
+      const findProductService = new FindProductService()
+
+      products = await findProductService.execute({ idProduct: idArray })
     }
 
-    return res.render("index", {page: "carrinho",libs:['carrinho'] ,styles: ["carrinho"], username: req.user_name, products});
-        
-    }
+    return res.render('index', {
+      page: 'carrinho',
+      libs: ['carrinho'],
+      styles: ['carrinho'],
+      username: req.user_name,
+      products
+    })
+  }
 }
