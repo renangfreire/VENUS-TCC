51fda3b27926a055daca8c0bdc471ba2b343c02c
Renan Freire <renangomesfreire@gmail.com>
2022-10-17 19:13:01 -0300

    progress in script payment - notebook to pc

--- src/controller/PaymentController.js
+++ src/controller/PaymentController.js
@@ -3,10 +3,26 @@ const PaymentService = require('../service/PaymentService')
 
 module.exports = {
     async handle(req, res) {
+        const payment_data = {
+            transaction_amount: Number(req.body.transactionAmount),
+            token: req.body.token,
+            description: req.body.description,
+            installments: Number(req.body.installments),
+            payment_method_id: req.body.paymentMethodId,
+            issuer_id: req.body.issuer,
+            payer: {
+              email: req.body.email,
+              identification: {
+                type: req.body.identificationType,
+                number: req.body.identificationNumber
+              }
+            }
+          };
+          
         const paymentService = new PaymentService()
 
-        paymentService.execute()
+        await paymentService.execute({payment_data, res})
 
-        return res.json({"message": "successfully"})
+        // return res.json({"message": "successfully"})
     }
 }
